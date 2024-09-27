e6ecfe92fa8fd1e88931f2cc8d49405647766cd1
shauryasds <119954061+shauryasds@users.noreply.github.com>
2024-09-27 12:35:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,27 +1,12 @@
 const dotenv = require("dotenv")
 dotenv.config()
 const cookieParser = require('cookie-parser');
+
 const express = require('express');
 const mongoose = require('mongoose');
+var cors = require('cors')
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
-// Define the allowCors function
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', 'https://mern-resturant-application1-git-master-shaurya-deeps-projects.vercel.app'); // Set your specific origin
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
-// Connect to MongoDB
 async function connectDb(){
   if (!DB_PASSWORD) {
     console.error('DB_PASSWORD environment variable is not set');
@@ -35,21 +20,29 @@ async function connectDb(){
   }
 }
 
-// Start the server
 async function startServer() {
   await connectDb();
   const app = express();
 
+ app.use(cors({
+  origin: 'https://mern-resturant-application1-git-master-shaurya-deeps-projects.vercel.app',
+  credentials: true, // Allow cookies and other credentials
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Content-Type', 'Accept'],
+}));
+  
   app.use(cookieParser());
   app.use(express.json());
   const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
   const router = require('./routes/index');
   const port = process.env.PORT_NAME;
+  
 
-  // Wrap the router with the allowCors function
-  app.use('/api', allowCors(router));
 
+
+  
+  app.use('/api', router);
   app.listen(port, () => {
     console.log(` listening on port ${port}`);
   });
