d0285688b9434e1443e00638fb7d42654d2f12cb
shauryasds <119954061+shauryasds@users.noreply.github.com>
2024-09-27 17:33:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,9 +24,9 @@ async function startServer() {
   await connectDb();
   const app = express();
 
-//  app.use(cors({
-  origin: ['https://mern-resturant-application1-git-master-shaurya-deeps-projects.vercel.app'], // allow requests from this origin
-  credentials: true, // allow credentials (e.g., cookies) to be sent
+app.use(cors({
+  origin: ['https://mern-resturant-application1-git-master-shaurya-deeps-projects.vercel.app'], 
+  credentials: true
 };));
      
   app.use(cookieParser());
@@ -36,13 +36,7 @@ async function startServer() {
   const router = require('./routes/index');
   const port = process.env.PORT_NAME;
   
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://mern-resturant-application1-git-master-shaurya-deeps-projects.vercel.app');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-  next();
-});
+
 
 
   
