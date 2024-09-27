867eee2b3fe23abdb1c915cf38176b7ccde85eb6
shauryasds <119954061+shauryasds@users.noreply.github.com>
2024-09-27 12:56:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,7 +38,13 @@ async function startServer() {
   const router = require('./routes/index');
   const port = process.env.PORT_NAME;
   
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://mern-resturant-application1-git-master-shaurya-deeps-projects.vercel.app');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  next();
+});
 
 
   
