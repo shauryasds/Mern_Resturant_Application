cc1489962677829155cf8fe2fe79be9417307ea9
shauryasds <119954061+shauryasds@users.noreply.github.com>
2024-09-27 19:34:10 +0530

    Update login.js

--- server/login.js
+++ server/login.js
@@ -23,11 +23,11 @@ async function login(req, res) {
        
 
         // Send the token to the client
-        // res.status(200).cookie('user',token).json({success:true,message:"logged in",body:user,error:false});
-        res.status(200)
-            .setHeader('Set-Cookie', `user=${token}; HttpOnly; Secure; SameSite=None; Max-Age=3600`)
-            .json({ success: true, message: "logged in", body: user, error: false });
-
+       res.status(200).cookie('user', token, {
+  secure: true, // Set the cookie to secure
+  httpOnly: true, // Set the cookie to HTTP only
+  sameSite: 'none' // Set the same-site policy to strict
+}).json({ success: true, message: "logged in", body: user, error: false });
 
     } catch (error) {
         console.error('Login error:', error);
