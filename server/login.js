7be0b4d87ebef1200a7ba80e4be00addc1821f59
shauryasds <119954061+shauryasds@users.noreply.github.com>
2024-09-28 10:07:34 +0530

    Update login.js

--- server/login.js
+++ server/login.js
@@ -17,8 +17,13 @@ async function login(req, res) {
        
 
         // Send the token to the client
-      res.status(200).setHeader('Set-Cookie', `user=${token}; Secure; SameSite="None"; Domain=frontend-resturant.vercel.app; Path=/;  Max-Age=3600000`).json({ success: true, message: "logged in", body: user, error: false });  } catch (error) {
-        console.error('Login error:', error);
+     res.cookie('user', token, {
+  secure: true,
+  sameSite: 'None',
+  domain: 'frontend-resturant.vercel.app',
+  path: '/',
+  maxAge: 3600000
+}).status(200).json({ success: true, message: "logged in", body: user, error: false });  console.error('Login error:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
 }
