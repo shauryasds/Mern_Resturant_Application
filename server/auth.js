4fbc7120399931cc44c569a122a85e1ebef233c6
shauryasds <119954061+shauryasds@users.noreply.github.com>
2024-09-29 20:35:02 +0530

    Update auth.js

--- server/auth.js
+++ server/auth.js
@@ -1,7 +1,7 @@
 const jwt=require('jsonwebtoken');
 const User = require('./models/UserModel');
 async function auth(req, res, next) {
-    const JWT_SECRET_KEY = "136546x4v4x4b654xcv4bvc3!@!@^&*3x54b5sd4db46vc4xb6" || process.env.JWT_SECRET_KEY;
+    const JWT_SECRET_KEY =  process.env.JWT_SECRET_KEY;
     try {
       const token = req.cookies['user'];
       console.log('Token:', token);
@@ -10,7 +10,7 @@ async function auth(req, res, next) {
         const decoded = await jwt.verify(token, JWT_SECRET_KEY);
         console.log('Decoded:', decoded);
   
-        const result = await User.findOne({ email: decoded.data.email });
+        const result = await User.findOne({ email: decoded.email });
         console.log('Result:', result);
   
         if (result !== null && result.role === 'admin') {
